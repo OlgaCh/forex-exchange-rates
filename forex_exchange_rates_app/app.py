462fc42d3561137d293f54dc34bb16fe31267388
Olga Chebotaryova <olga.chebotaryova@gmail.com>
2019-02-10 15:56:19 +0300

    DRYning the code.

--- forex_exchange_rates_app/app.py
+++ forex_exchange_rates_app/app.py
@@ -1,23 +1,14 @@
-import redis
-import logging
-
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import desc
 
-from config.settings import MYSQL_URI, REDIS_HOST, REDIS_PORT, REDIS_DB, REDIS_PWD
-from models.models import ExchangeRate
-from utils.grab_and_save import grab_and_save_rate
+from config.settings import MYSQL_URI
+from utils.grab_and_save import grab_and_save_rate, jsonify_data
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = MYSQL_URI
 
 db = SQLAlchemy(app)
 session = db.session
-
-# setting up Redis connection
-redis_db = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB,
-                             password=REDIS_PWD, decode_responses=True)
 
 
 # POST Request to get exchange rates
@@ -40,19 +31,7 @@ def get_last():
     """
     Return last operation stored in MySQL and Redis
     """
-    # MySQL
-    last_ms = session.query(ExchangeRate).order_by(desc(ExchangeRate.timestamp)).first()
-    # Redis
-    keys = redis_db.keys()
-    keys.sort(reverse=True)
-
-    if not len(keys):
-        logging.warning('Exchange rates were requested. No data in database.')
-        return jsonify(APIWarning='There is no exchange rates in database')
-
-    last_redis = redis_db.hgetall(keys[0])
-
-    return jsonify(MySQL=last_ms.serialize, Redis=last_redis)
+    return jsonify_data()
 
 # GET request to show last N exchange operations
 @app.route('/last/<int:n>', methods=['GET'])
@@ -61,20 +40,7 @@ def get_last_n(n):
     """
     Return last operation stored in MySQL and Redis
     """
-    # MySQL
-    last_ms = session.query(ExchangeRate).order_by(desc(ExchangeRate.timestamp)).limit(n)
-    # Redis
-    keys = redis_db.keys()
-    keys.sort(reverse=True)
-
-    exchange_count = len(keys)
-    if exchange_count < n:
-        logging.warning(f'Requested last {n} exchanges. Data only for {exchange_count} rates present')
-        return jsonify(APIWarning=f'Too large number of exchange rates. Only '
-                                  f'{exchange_count} rates present')
-
-    return jsonify(MySQL=[ms.serialize for ms in last_ms],
-                   Redis=[redis_db.hgetall(keys[i]) for i in range(n)])
+    return jsonify_data(n=n)
 
 
 # GET request to show last exchange operation for specific currency
@@ -84,20 +50,7 @@ def get_last_currency(currency):
     """
     Return last operation stored in MySQL and Redis
     """
-    # MySQL
-    last_ms = session.query(ExchangeRate).filter(ExchangeRate.currency == currency)\
-        .order_by(desc(ExchangeRate.timestamp)).first()
-    # Redis
-    keys = [k for k in redis_db.keys() if currency in k]
-    keys.sort(reverse=True)
-
-    if not len(keys):
-        logging.warning(f'Exchange rates for {currency} were requested. No data in database.')
-        return jsonify(APIWarning=f'There is no exchange rates for {currency} in database')
-
-    last_redis = redis_db.hgetall(keys[0])
-
-    return jsonify(MySQL=last_ms.serialize, Redis=last_redis)
+    return jsonify_data(currency=currency)
 
 
 # GET request to show last N exchange operations for specific currency
@@ -107,22 +60,7 @@ def get_last_n_currency(currency, n):
     """
     Return last operation stored in MySQL and Redis
     """
-    # MySQL
-    last_ms = session.query(ExchangeRate).filter(ExchangeRate.currency == currency).\
-        order_by(desc(ExchangeRate.timestamp)).limit(n)
-    # Redis
-    keys = [k for k in redis_db.keys() if currency in k]
-    keys.sort(reverse=True)
-
-    exchange_count = len(keys)
-    if exchange_count < n:
-        logging.warning(f'Requested last {n} exchanges for {currency}. Data only for {exchange_count}'
-                        f' rates present')
-        return jsonify(APIWarning=f'Too large number of exchange rates for {currency}. '
-                                  f'Only {exchange_count} rates present')
-
-    return jsonify(MySQL=[ms.serialize for ms in last_ms],
-                   Redis=[redis_db.hgetall(keys[i]) for i in range(n)])
+    return jsonify_data(n=n, currency=currency)
 
 
 if __name__ == '__main__':
--- utils/grab_and_save.py
+++ utils/grab_and_save.py
@@ -3,7 +3,10 @@ import logging
 import redis
 from time import time
 
+from flask import jsonify
+
 from sqlalchemy import create_engine
+from sqlalchemy import desc
 from sqlalchemy.orm import Session
 
 from config.settings import OXR_APIKEY, DEFAULT_BASE, MYSQL_URI, REDIS_HOST, REDIS_PORT, \
@@ -17,8 +20,8 @@ session = Session(engine)
 
 
 # setting up Redis connection
-redis_db = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, password=REDIS_PWD)
-
+redis_db = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB,
+                             password=REDIS_PWD, decode_responses=True)
 
 def get_oxr_rate():
     """
@@ -93,7 +96,93 @@ def save_to_redis(rate):
 
 
 def grab_and_save_rate(currency, original_amount):
+    """
+    Call OXR API with provided values and save output to MySQL and Redis
+    :param currency: str - 3-letter code: GBP, EUR.
+        `currency` can't be USD since we converting from it.
+    :param original_amount: float - - amount in USD which needs to be converted.
+    """
     rate = limit_to_currency(currency)
     calculate_final_amount(rate, original_amount)
     save_to_mysql(rate)
     save_to_redis(rate)
+
+
+def get_from_my_sql(n=None, currency=None):
+    """
+    Query MySQL to get last operations which match criteria.
+    :param n: int - number of operations
+    :param currency: str - 3-letter code. Currency of exchange
+    :return: list - operations performed.
+    """
+    if n is None:
+        n = 1
+    if currency:
+        ms_data = session.query(ExchangeRate).filter(ExchangeRate.currency == currency)\
+        .order_by(desc(ExchangeRate.timestamp)).limit(n)
+    else:
+        ms_data = session.query(ExchangeRate).order_by(desc(ExchangeRate.timestamp)).limit(n)
+    return [ms.serialize for ms in ms_data]
+
+
+def get_from_redis(n=None, currency=None):
+    """
+    Query Redis to get last operations which match criteria.
+    :param n: int - number of operations
+    :param currency: str - 3-letter code. Currency of exchange
+    :return: list - operations performed.
+    """
+    if n is None:
+        n = 1
+    if currency:
+        keys = [k for k in redis_db.keys() if currency in k]
+    else:
+        keys = [k for k in redis_db.keys()]
+    keys.sort(reverse=True)
+    return [redis_db.hgetall(keys[i]) for i in range(n)]
+
+def jsonify_data(n=None, currency=None):
+    """
+    Return jsonified response to be shown as API response
+    :param n: int - number of operations
+    :param currency: str - 3-letter code. Currency of exchange
+    :return: json
+    """
+    validation_error = validate_data(n, currency)
+    if validation_error:
+        return validation_error
+    return jsonify(MYSQL=get_from_my_sql(n, currency),
+                   Redis=get_from_redis(n, currency))
+
+
+def validate_data(n=None, currency=None):
+    """
+    Validates user input to see if we have enough data for it.
+    :param n: int - number of operations
+    :param currency: str - 3-letter code. Currency of exchange
+    :return: json - JSON error message to be shown for user.
+    """
+    total_currency_records = 0
+    if currency:
+        total_currency_records = session.query(ExchangeRate).\
+            filter(ExchangeRate.currency == currency).count()
+    total_records = session.query(ExchangeRate).count()
+    if n and currency and total_currency_records < n:
+        logging.warning(f'Requested last {n} exchanges for {currency}. Data only for '
+                        f'{total_currency_records}'
+                        f' rates present')
+        return jsonify(APIWarning=f'Too large number of exchange rates for {currency}. '
+                                  f'Only {total_currency_records} rates present')
+    elif n and total_records < n:
+        logging.warning(f'Requested last {n} exchanges. Data only for {total_records} '
+                        f'rates present')
+        return jsonify(APIWarning=f'Too large number of exchange rates. Only '
+                                  f'{total_records} rates present')
+    elif currency and total_currency_records == 0:
+        logging.warning(f'Exchange rates for {currency} were requested. No data in database.')
+        return jsonify(APIWarning=f'There is no exchange rates for {currency} in database')
+    elif total_records == 0:
+        logging.warning('Exchange rates were requested. No data in database.')
+        return jsonify(APIWarning='There is no exchange rates in database')
+    else:
+        return {}
